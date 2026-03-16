9c1f5e78d69c515054434b9079f1ba697c1cad53
andyMattick <133983456+andyMattick@users.noreply.github.com>
2026-03-16 22:00:16 +0000

    fake message

--- src/api/templates.ts
+++ src/api/templates.ts
@@ -7,6 +7,6 @@ export async function getTemplates(teacherId: string) {
   if (!res.ok) {
     throw new Error(`Failed to load templates: ${res.statusText}`);
   }
-
+//console.log("[getTemplates] Response data:", await res.clone().json()); // Log the response data for debugging
   return res.json();
 }
